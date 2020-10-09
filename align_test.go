62af1f0cfd9e00fb798bd625d1862bb3c85e418d
vejnar <charles.vejnar@gmail.com>
2020-10-09 09:22:59 -0400

    Add cost-free end gaps option

--- align_test.go
+++ align_test.go
@@ -10,12 +10,18 @@ import (
 
 func TestAlign(t *testing.T) {
 	seqs := [][]string{
-		[]string{"CGAGAGA", "GAGAGA", "CGAGAGA", "-GAGAGA"}}
+		[]string{"CGAGAGA", "GAGAGA", "CGAGAGA", "-GAGAGA", "CGAGAGA", "-GAGAGA"},
+		[]string{"CGAGAGA", "GACC", "CGAGAGA", "-GACC--", "CGAGAGA--", "-----GACC"},
+	}
 
 	for _, a := range seqs {
-		aln1, aln2, _ := Align(a[0], a[1], 1, -1, -1)
+		aln1, aln2, _ := Align(a[0], a[1], 1, -1, -1, false)
 		if aln1 != a[2] || aln2 != a[3] {
 			t.Errorf("Align(%s, %s)\n***GOT***\n%s\n%s\n***WANT***\n%s\n%s", a[0], a[1], aln1, aln2, a[2], a[3])
+		}
+		aln1, aln2, _ = Align(a[0], a[1], 1, -1, -1, true)
+		if aln1 != a[4] || aln2 != a[5] {
+			t.Errorf("Align(%s, %s)\n***GOT***\n%s\n%s\n***WANT***\n%s\n%s", a[0], a[1], aln1, aln2, a[4], a[5])
 		}
 	}
 
@@ -26,6 +32,6 @@ func BenchmarkAlign(b *testing.B) {
 	seq2 := "GCCAGGATTCCCAGATATGGCCAAGGTTCC"
 
 	for i := 0; i < b.N; i++ {
-		Align(seq1, seq2, 1, -1, -1)
+		Align(seq1, seq2, 1, -1, -1, false)
 	}
 }
